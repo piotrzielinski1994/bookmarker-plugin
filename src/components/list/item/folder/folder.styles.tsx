06a0cd2353819ab5bb19451a33dc234858394722
piotr.zielinski <piotr.zielinski@sidnet.info>
2020-06-28 22:52:00 +0200

    Add hover effect to folder's button

--- src/components/list/item/folder/folder.styles.tsx
+++ src/components/list/item/folder/folder.styles.tsx
@@ -29,6 +29,13 @@ export const StyledButton = styled.button<StyledButtonProps>`
   align-items: center;
   justify-content: center;
   cursor: pointer;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 3px -2px,
+    rgba(0, 0, 0, 0.14) 0px 3px 4px 0px, rgba(0, 0, 0, 0.12) 0px 1px 8px 0px;
+
+  &:hover {
+    background-color: #eeeeee;
+  }
 `;
 
 export const StyledFolder = styled.div<StyledFolderProps>`
